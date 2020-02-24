e850340b72b6f227e193d004ffb3672d8ca86ce2
nahon1 <60366732+nahon1@users.noreply.github.com>
2020-02-24 17:42:34 -0500

    edits
    complete

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -7,3 +7,12 @@ func _ready():
 
 func _physics_process(delta):
 	pass
+	if Input.is_action_pressed("ui_left"):
+    position.x = position.x - 10
+	if Input.is_action_pressed("ui_right"):
+    position.x = position.x + 10
+	if Input.is_action_pressed("ui_up"):
+    position.y = position.y - 10
+	if Input.is_action_pressed("ui_down"):
+    position.y = position.y + 10
+	$Camera.add_trauma(0.1)
